208199636025fd973743252cfe66db8a85a976e0
Daniel Tan <danieltan380@gmail.com>
2023-09-18 19:28:17 -0700

    Updated the return statements to use template literals instead of using + to concatenate values

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return "✅✅✅ Assertion Passed: " + actual + " === " + expected;
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
   }
   if (actual !== expected) {
-    return "🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected;
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
   }
 };
 
