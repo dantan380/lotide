f567b66d0d1312f46b11d3ea7d48e914f3aa663f
Daniel Tan <danieltan380@gmail.com>
2023-09-20 23:12:15 -0700

    Linted

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,7 +1,7 @@
-const assertObjectsEqual = function (actual, expected){
-    const inspect = require('util').inspect; //*
-    if (inspect(actual) === inspect(expected)){
-        return `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`;
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect; //*
+  if (inspect(actual) === inspect(expected)) {
+    return `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`;
   } else {
     return `🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`;
   }
