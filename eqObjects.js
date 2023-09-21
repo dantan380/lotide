3c1c50790daf11b3d3573fb1586831365c3ee242
Daniel Tan <danieltan380@gmail.com>
2023-09-20 21:52:44 -0700

    Further defined the function eqObjects. Added a check to see if both objects had the same number of keys, as well as see if the values of those keys match

--- eqObjects.js
+++ eqObjects.js
@@ -7,5 +7,22 @@ const assertEqual = function(actual, expected) {
   };
 
   const eqObjects = function (object1, object2){
-    
+     const shirtObjectArr = Object.keys(shirtObject)
+     const anotherShirtObjectArr = Object.keys(anotherShirtObject)
+     if (shirtObjectArr.length === anotherShirtObjectArr.length){
+        for (let key of shirtObjectArr){
+            if(shirtObject[key] === anotherShirtObject[key]){
+                return true;
+            } else {
+                return false;
+            }
+        }
+     }
   }
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject= { size: "medium", color: "red" };
+console.log(assertEqual(eqObjects(shirtObject , anotherShirtObject), true));
+
+const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+console.log(assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false));
