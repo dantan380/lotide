ae4d4db67d37c76e5aa07b61d744c02b4c0e6e74
Daniel Tan <danieltan380@gmail.com>
2023-09-21 19:41:54 -0700

    Added tester code and some comments

--- takeUntil.js
+++ takeUntil.js
@@ -13,3 +13,30 @@ const assertArraysEqual = function(actual, expected) {
       return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
     }
   };
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //Tester code:
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0); //Callback function evaluates if "x" is greater than 0.
+console.log(results1);
+
+
+console.log(assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]));
+
+//Callback function of data2 evaluates if "x" is equal to ",".
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+console.log(assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]));
