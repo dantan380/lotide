72728e1af1c6a545128c1b48bddd6a8c0c681c82
Daniel Tan <danieltan380@gmail.com>
2023-09-21 20:07:32 -0700

    Defined findKey function using similar functionality from findKeyByValue. Also added some code to test the function

--- findKey.js
+++ findKey.js
@@ -6,3 +6,25 @@ const assertEqual = function(actual, expected) {
       return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
     }
   };
+
+  const findKey = function(object, callback){
+    const objectKeys = Object.keys(object);
+    console.log(objectKeys);
+    for (let key of objectKeys){
+        if(!callback(object[key])){
+            continue
+        } else {
+            return key;
+        }
+    }
+  }
+
+  const result1 = findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 2)
+  console.log(result1);
