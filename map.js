1dca8e98f4bf39603bbc49d6a23c806ab0e19bc9
Daniel Tan <danieltan380@gmail.com>
2023-09-21 18:40:23 -0700

    Completed the logic for the 'map' function by pushing to the results array. Also added assertArraysEqual for testing

--- map.js
+++ map.js
@@ -1,8 +1,27 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
+const assertArraysEqual = function(actual, expected) {
+    const eqArrays = function(expectedArr, actualArr) {
+      for (let i = 0; i < expectedArr.length; i++) {
+        if (expectedArr[i] !== actualArr[i]) {
+          return false;   //If anyone of the elements in the array don't match in value and type, return false.
+        }
+      }            //If all the elements don't meet the requirements for the conditional statement, return true.
+      return true;
+    };
+    if (eqArrays(actual, expected) === true) {
+      return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+    } else {
+      return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+    }
+  };
+
 
 const map = function (array, callback){
     const results = [];
+    for (let item of array){
+        results.push(callback(item));
+    }
     return results;
 }
 
