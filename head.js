ef99125162481832e7889ed06682a5c212ff3c58
Daniel Tan <danieltan380@gmail.com>
2023-09-25 13:22:25 -0700

    Refactored to include require to import assertEqual. Also added module.exports for test code file

--- head.js
+++ head.js
@@ -5,6 +5,4 @@ const head = function(array) {
   return array[0];
 };
 
-//Test Code
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+module.exports = head;
