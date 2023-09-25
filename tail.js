b1a4b5401c9dd930d571083f16835350d166e5e2
Daniel Tan <danieltan380@gmail.com>
2023-09-25 13:44:03 -0700

    Refactored to to import assertEqual via require function.

--- tail.js
+++ tail.js
@@ -1,3 +1,6 @@
+const assertEqual = require('./assertEqual');
+
+
 const tail = function(array) {
   const newArray = array.slice(1);
   return newArray;
@@ -6,16 +9,4 @@ const tail = function(array) {
 //Array.slice() is used to create a new array with the values from "array" with the starting index position
 // of "1", which skips over the head of the array.
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  }
-  if (actual !== expected) {
-    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-  }
-};
-
-// Test code to check if "tail" is not modifying the original array.
-const words = ["hey", "there", "stranger"];
-console.log(tail(words));
-console.log(assertEqual(words.length, 3));
+module.exports = tail;
