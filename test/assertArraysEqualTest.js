d740063b2b2d668a53074bcb4dd1808164519575
Daniel Tan <danieltan380@gmail.com>
2023-09-25 15:11:01 -0700

    Used require to import assertArraysEqual. Added tester code

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,5 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+//Tester code:
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+console.log(assertArraysEqual([3, 3, 3], [3, 3, 3]));
