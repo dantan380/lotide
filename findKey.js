67c2a500f4ba1ef8641497dc2ff46a1e47c37112
Daniel Tan <danieltan380@gmail.com>
2023-09-25 19:29:57 -0700

    Imported assertEqual with require. Seperated test code. Exported to test file

--- findKey.js
+++ findKey.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  }
-  if (actual !== expected) {
-    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-  }
-};
-
+const assertEqual = require('./assertEqual');
 const findKey = function(object, callback) {  //findKey will take in an object and a callback as parameters.
   const objectKeys = Object.keys(object); //objectKeys will contain all keys from the given object.
   for (let key of objectKeys) {  //For every key...
@@ -17,24 +9,4 @@ const findKey = function(object, callback) {  //findKey will take in an object a
     }                           //return that key.
   }
 };
-
-const result1 = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2);
-console.log(result1);
-
-console.log(assertEqual(result1, "noma"));
-
-const result2 = findKey({
-  "Patrick": {color: "pink"},
-  "Spongebob": {color: "yellow"},
-  "Squidward": {color: "blue"}
-}, x => x.color === "yellow");
-console.log(result2);
-
-console.log(assertEqual(result2, "Spongebob"));
+module.exports = findKey;
