00b77969ea19ac8136283cec52ac2f7c662bd9cd
Daniel Tan <danieltan380@gmail.com>
2023-09-25 19:57:21 -0700

    Imported assertArraysEqual with require. Removed test code. Exported to test file

--- takeUntil.js
+++ takeUntil.js
@@ -1,19 +1,4 @@
-const assertArraysEqual = function(actual, expected) {
-  const eqArrays = function(expectedArr, actualArr) {
-    for (let i = 0; i < expectedArr.length; i++) {
-      if (expectedArr[i] !== actualArr[i]) {
-        return false;
-      }
-    }
-    return true;
-  };
-  if (eqArrays(actual, expected) === true) {
-    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-  }
-};
-
+const assertArraysEqual = require('./assertArraysEqual');
 
 
 const takeUntil = function(array, callback) {
@@ -28,20 +13,4 @@ const takeUntil = function(array, callback) {
   }
   return taken;       // Return the "taken" array.
 };
-
-
-  
-//Tester code:
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0); //Callback function evaluates if "x" is greater than 0.
-console.log(results1);
-
-
-console.log(assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]));
-
-//Callback function of data2 evaluates if "x" is equal to ",".
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-console.log(assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]));
+module.exports = takeUntil;
