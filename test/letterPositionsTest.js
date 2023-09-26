657df92415e7c15548e4b63db4315756ca33ef07
Daniel Tan <danieltan380@gmail.com>
2023-09-25 19:40:20 -0700

    Imported assertArraysEqual and letterPositions with require. Added test code

---
+++ test/letterPositionsTest.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const letterPositions = require('../letterPositions');
+
+
+//Tester code:
+console.log(assertArraysEqual(letterPositions("hello").e, [1]));
+console.log(assertArraysEqual(letterPositions("hello").h, [0]));
