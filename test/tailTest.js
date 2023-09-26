195c038999cc1cbf55f43ceacf065a4394cfa19a
Daniel Tan <danieltan380@gmail.com>
2023-09-25 17:58:52 -0700

    Refactored to use mocha and chai. Imported assert with chai. Used describe and it functions from mocha for test cases.

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,19 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test code to check if "tail" is not modifying the original array.
+describe("#tail", () => {
+    it("returns ['there', 'stranger'] for ['hey', 'there', 'stranger']", () => {
+      assert.deepEqual(tail(["hey","there", "stranger"]), ["there","stranger"]);
+    });
+
+    it("returns [2, 3] for [1, 2, 3]", () => {
+        assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+    })
+  
+  });
+
+
+/*Test code to check if "tail" is not modifying the original array.
 const words = ["hey", "there", "stranger"];
 console.log(tail(words));
-console.log(assertEqual(words.length, 3));
+console.log(assertEqual(words.length, 3));*/
