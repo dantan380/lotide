ff34ca10e221915c576cbd26b29d17d675728d9a
Daniel Tan <danieltan380@gmail.com>
2023-09-25 18:51:26 -0700

    Exported for test file with module.exports

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,15 +1,15 @@
 const assertObjectsEqual = function(actual, expected) {
-  const inspect = require('util').inspect; //*
+  const inspect = require('util').inspect; 
   if (inspect(actual) === inspect(expected)) {
     return `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`;
   } else {
     return `🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`;
   }
 };
+
+module.exports = assertObjectsEqual;
     
-const poo = {poopy: 1};
-const poopy = {poopy: 1};
-console.log(assertObjectsEqual(poo, poopy));
+
 
 //* Util library's inspect function:
 //  Allows the keys of the object to be returned instead of [object Object] === [object Object].
